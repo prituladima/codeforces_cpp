aac673f0637072b4ccbfec97ed990321398bd289
prituladima <prituladima@gmail.com>
2018-07-29 18:48:52 +0300

    [a2oj] 5 - Codeforces Div. 2, B 2

--- a2oj/div2B/Problem2.cpp
+++ a2oj/div2B/Problem2.cpp
@@ -5,9 +5,26 @@ using namespace std;
 
 #include <iostream>
 
-int main1() {
+int main() {
 
-    cout << "HW" << endl;
+    int n, m;
+    cin >> n >> m;
+    int deals[m + 1];
+    deals[0] = 1;
+    for (int i = 1; i < m + 1; i++) {
+        cin >> deals[i];
+    }
 
+    long long counter = 0;
+    for (int i = 1; i < m + 1; i++) {
+        if (deals[i] >= deals[i - 1]) {
+            counter += deals[i] - deals[i - 1];
+        } else {
+            counter += n - deals[i - 1];
+            counter += 1;
+            counter += deals[i] - deals[0];
+        }
+    }
+    cout << counter << endl;
     return 0;
 }
