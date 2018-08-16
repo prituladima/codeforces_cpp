aa6f92da29ee9f67a783740a7c3eedfd3f622062
prituladima <prituladima@gmail.com>
2018-08-16 18:02:35 +0300

    [a2oj] 5 - Codeforces Div. 2, B 21

---
+++ a2oj/div2B/Problem21.cpp
@@ -0,0 +1,39 @@
+//
+// Created by prituladima on 8/16/18.
+//
+
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define foreach(cur, map) for(pair<char, int> cur : map)
+#define ll long long
+
+using namespace std;
+
+int main() {
+
+    ll n;
+    cin >> n;
+    ll m;
+    cin >> m;
+    ll d[m];
+    forn(i, m) cin >> d[i];
+
+    sort(d, d + m);
+
+    if (d[0] == 1 || d[m - 1] == n) {
+        cout << "NO";
+        return 0;
+    }
+
+    if (n > 4) {
+        for (int j = 0; j < m - 2; ++j) {
+            if (d[j] + 1 == d[j + 1] && d[j + 1] + 1 == d[j + 2]) {
+                cout << "NO";
+                return 0;
+            }
+        }
+    }
+    cout << "YES";
+    return 0;
+}
