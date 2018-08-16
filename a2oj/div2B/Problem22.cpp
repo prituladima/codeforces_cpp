6c637a1bc6edd4ca3f890e43ee9c688839281583
prituladima <prituladima@gmail.com>
2018-08-16 18:32:09 +0300

    [a2oj] 5 - Codeforces Div. 2, B 22

---
+++ a2oj/div2B/Problem22.cpp
@@ -0,0 +1,35 @@
+//
+// Created by prituladima on 8/16/18.
+//
+#include <bits/stdc++.h>
+#include <algorithm>
+
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define foreach(cur, map) for(pair<char, int> cur : map)
+#define ll long long
+
+using namespace std;
+
+int main() {
+
+    ll n, t;
+    cin >> n >> t;
+
+    ll a[n];
+    forn(i, n) cin >> a[i];
+    ll sum = 0;
+    ll max_amount_of_books = 0;
+    int last_ = 0;
+    forn(i, n) {
+        sum += a[i];
+//        cout << sum << " ";
+        while (sum > t) {
+            sum -= a[last_++];
+//            cout << " -w- " << sum << " ";
+        }
+
+        max_amount_of_books = max(max_amount_of_books, (ll)(i + 1 - last_));
+    }
+    cout << max_amount_of_books;
+    return 0;
+}
