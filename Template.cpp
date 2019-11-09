2822dc623b4e87f80ae50e59ad7c4b4e819a2ed2
Dmytro Prytula <prituladima@gmail.com>
2019-11-09 21:07:44 +0200

    Template

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dmytro Prytula on 09.11.2019.
+//
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //solution
+
+    return 0;
+}
